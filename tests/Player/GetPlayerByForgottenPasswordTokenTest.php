f0815ee5baf43e6ada3ac4d7cf531f491eca7b2e
TBoileau <t-boileau@email.com>
2022-09-01 16:06:00 +0200

    test (player): ajouter du test pour vérifier que GetPlayerByForgottenPasswordToken retourne null

--- tests/Player/GetPlayerByForgottenPasswordTokenTest.php
+++ tests/Player/GetPlayerByForgottenPasswordTokenTest.php
@@ -33,6 +33,19 @@ final class GetPlayerByForgottenPasswordTokenTest extends QueryTestCase
         self::assertInstanceOf(Player::class, $player);
     }
 
+    public function testShouldReturnNull(): void
+    {
+        /** @var PlayerGateway $playerGateway */
+        $playerGateway = $this->container->get(PlayerGateway::class);
+
+        /** @var UuidGeneratorInterface $uuidGenerator */
+        $uuidGenerator = $this->container->get(UuidGeneratorInterface::class);
+
+        $player = $this->queryBus->fetch(new ForgottenPasswordToken((string) $uuidGenerator->generate()));
+
+        self::assertNull($player);
+    }
+
     public function testShouldFailedDueToAExpiredForgottenPasswordRequest(): void
     {
         /** @var PlayerGateway $playerGateway */
